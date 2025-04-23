8fa2927324dcbb5c23a85a9bfe941839b554898b
Malik <malik_zainoldin@mail.ru>
2025-04-23 14:22:08 +0500

    index.html mod

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+//alert('hello');
+
+//const  result = confirm('are you here?');
+//console.log(result);
+
+//const answer= prompt("Вам есть 18?", "18" );
+//console.log(answer);
+
+//const answers = [];
+
+//answers[0]= prompt('как ваше имя?','');
+//answers[1]= prompt('как ваше фамилия?','');
+//answers[2]= prompt('сколько вам лет?','');
+
+//document.write(answers);
+
+ //const category = 'toys'
+ //console.log(`https://someurl.com/${category}/5`)
+
+ //const user = "Ivan";
+// alert(`Привет,${user}`) 
+ 
+console.log('arr'+ "- object");
+console.log(4 + "5");
+
+let incr = 10, 
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+const isChecked = true;
+      isClose = true;
+
+console.log(isChecked && isClose);      
